aee2472df95d0716e0fd43de302dcaddf543efb4
Roiy Zysman <Roiy Zysman>
2017-03-19 21:15:23 -0700

    Remove unusable code

--- Program.cs
+++ Program.cs
@@ -121,31 +121,6 @@ namespace SentimentAnalyzer
 
         }
 
-        private static async Task TranslateAsync()
-        {
-            var translatorService = new TranslatorService.LanguageServiceClient();
-            var authTokenSource = new AzureAuthToken(SubscriptionKey);
-            var token = string.Empty;
-
-            try
-            {
-                token = await authTokenSource.GetAccessTokenAsync();
-            }
-            catch (HttpRequestException)
-            {
-                switch (authTokenSource.RequestStatusCode)
-                {
-                    case HttpStatusCode.Unauthorized:
-                        Console.WriteLine("Request to token service is not authorized (401). Check that the Azure subscription key is valid.");
-                        break;
-                    case HttpStatusCode.Forbidden:
-                        Console.WriteLine("Request to token service is not authorized (403). For accounts in the free-tier, check that the account quota is not exceeded.");
-                        break;
-                }
-                throw;
-            }
-
-            Console.WriteLine("Translated to French: {0}", translatorService.Translate(token, "Hello World", "en", "fr", "text/plain", "general", string.Empty));
-        }
+        
     }
 }
